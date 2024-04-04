fc54a85eee315130fd229f892512856012c473b1
leterou <letero@bk.ru>
2024-04-04 13:10:07 +0200

    lesson2PrintAndComments

---
+++ l2/src/main/kotlin/HW.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>) {
+    //My Name
+    print("Name: ")
+    print("Alexandr ")
+    println("Mikhaylovskiy")
+
+    /*
+    Where your from?
+    */
+    println("From: Kaliningrad")
+    println("Age: 34")
+    println("Prof: Programmer")
+    //println("Prof: Santehnik")
+}
