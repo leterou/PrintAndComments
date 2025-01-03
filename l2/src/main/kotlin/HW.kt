a4303574312921c0579629e13a3e1b3000f59d34
leterou <letero@bk.ru>
2025-01-03 13:27:53 +0200

    2025-1

---
+++ l2/src/main/kotlin/HW.kt
@@ -0,0 +1,16 @@
+fun main() {
+    /*About me:
+    -Name
+    -Age
+    -prof
+    -where from
+     */
+    println("Name - Alexandr")
+    println("SureName - Mikhaylovskiy")
+    println("From - Kaliningrad")
+    print("Age - 34 ")
+    println("y.o")
+    print("Prof - Programmer")
+    //println("Interest - VideoMaking")
+
+}
